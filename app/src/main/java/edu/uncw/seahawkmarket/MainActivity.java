a70b2d9f75090d632c64b902fa28f5d6fb1c0185
cms5377 <cms5377@uncw.edu>
2021-04-02 00:30:31 -0400

    Modified the package name

--- app/src/main/java/edu/uncw/seahawkmarket/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawkmarket/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package edu.uncw.seahawkmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import edu.uncw.seahawkmarket.R;
 
 public class MainActivity extends AppCompatActivity {
 
