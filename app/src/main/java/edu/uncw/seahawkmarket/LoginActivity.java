74c80fd6d82ae60636a737799f8cf12d2a373bc4
Jake Aldridge <jsa5664@uncw.edu>
2021-04-07 18:35:19 -0400

    Created appbar with correct app title, created login activity and layout.

---
+++ app/src/main/java/edu/uncw/seahawkmarket/LoginActivity.java
@@ -0,0 +1,20 @@
+package edu.uncw.seahawkmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Add toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
