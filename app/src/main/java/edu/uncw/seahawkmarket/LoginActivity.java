2d5d217a9241373006eaf29f49528c46938f2c0c
Jake Aldridge <jsa5664@uncw.edu>
2021-04-07 20:43:09 -0400

    Implemented firebase authentication features. Not fully functional yet.

--- app/src/main/java/edu/uncw/seahawkmarket/LoginActivity.java
+++ app/src/main/java/edu/uncw/seahawkmarket/LoginActivity.java
@@ -1,20 +1,128 @@
 package edu.uncw.seahawkmarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth auth;
+    private EditText emailField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+
+        emailField = findViewById(R.id.enterEmail);
+        passwordField = findViewById(R.id.enterPassword);
+
         //Add toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
+
+    //Check that email and password are valid
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailField.setError("Required.");
+            valid = false;
+        } else {
+            emailField.setError(null);
+        }
+
+        String password = passwordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordField.setError("Required.");
+            valid = false;
+        } else {
+            passwordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    public void signIn(View view){
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, launch main activity with intent and send user email
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra(MainActivity.userEmail, user.getEmail());
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Exception e = task.getException();
+                            Log.w(TAG, "signInWithEmail:failure", e);
+                            Toast.makeText(LoginActivity.this, "Login failed: " + e.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    public void createAccount(View view) {
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, start main activity with user email
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra(MainActivity.userEmail, user.getEmail());
+                            startActivity(intent);
+
+                        } else {
+                            // If registration fails, display a message to the user.
+                            Exception e = task.getException();
+                            Log.w(TAG, "createUserWithEmail:failure", e);
+                            Toast.makeText(LoginActivity.this, "Registration failed: " + e.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 }
--- app/src/main/java/edu/uncw/seahawkmarket/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawkmarket/MainActivity.java
@@ -2,15 +2,32 @@ package edu.uncw.seahawkmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import edu.uncw.seahawkmarket.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String userEmail = "";
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void signOut(View view) {
+        auth.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
