dcd7a1cf3a56cd5fe8c2d55574240986e22ef4af
cms5377 <79059899+cms5377@users.noreply.github.com>
2021-04-21 18:36:38 -0400

    Added input validation for creating a listing

--- app/src/main/java/edu/uncw/seahawkmarket/CreateListingActivity.java
+++ app/src/main/java/edu/uncw/seahawkmarket/CreateListingActivity.java
@@ -33,23 +33,40 @@ public class CreateListingActivity extends AppCompatActivity {
         String description = itemDescription.getText().toString();
         String priceString = itemPrice.getText().toString();
         String price = priceString;
-
-        ItemsForSale item = new ItemsForSale(title, description, price);
-        Log.d(TAG, "\nListed item: " + " \n Name of item: " + item.getTitle() + "\n Description: " + item.getDescription() + "\n price: " + item.getPrice());
-        mDb.collection("Items for sale").add(item).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Log.d(TAG, "Item added for sale successfully");
-                Toast.makeText(CreateListingActivity.this, "Item added for sell!", Toast.LENGTH_SHORT).show();
+        if(!title.isEmpty()&&!description.isEmpty()&&!price.isEmpty()) {
+            ItemsForSale item = new ItemsForSale(title, description, price);
+            Log.d(TAG, "\nListed item: " + " \n Name of item: " + item.getTitle() + "\n Description: " + item.getDescription() + "\n price: " + item.getPrice());
+            mDb.collection("Items for sale").add(item).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d(TAG, "Item added for sale successfully");
+                    Toast.makeText(CreateListingActivity.this, "Item added for sell!", Toast.LENGTH_SHORT).show();
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Item not added");
+                            Toast.makeText(CreateListingActivity.this, "Could not add item for sell.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+        else{
+            if(title.isEmpty()&&description.isEmpty()&&price.isEmpty()) {
+                Toast.makeText(CreateListingActivity.this, "Missing information for Name, Description, and Price", Toast.LENGTH_SHORT).show();
+            } else if (title.isEmpty()&&description.isEmpty()&&!price.isEmpty()){
+                Toast.makeText(CreateListingActivity.this, "Missing information for Name and Description", Toast.LENGTH_SHORT).show();
+            } else if (title.isEmpty()&&!description.isEmpty()&&!price.isEmpty()){
+                Toast.makeText(CreateListingActivity.this, "Missing information for Name", Toast.LENGTH_SHORT).show();
+            } else if (title.isEmpty()&&!description.isEmpty()&&price.isEmpty()){
+                Toast.makeText(CreateListingActivity.this, "Missing information for Name and Price", Toast.LENGTH_SHORT).show();
+            }  else if (!title.isEmpty()&&description.isEmpty()&&price.isEmpty()){
+                Toast.makeText(CreateListingActivity.this, "Missing information for Description and Price", Toast.LENGTH_SHORT).show();
+            } else if (!title.isEmpty()&&description.isEmpty()&&!price.isEmpty()){
+                Toast.makeText(CreateListingActivity.this, "Missing information for Description", Toast.LENGTH_SHORT).show();
+            } else if (!title.isEmpty()&&!description.isEmpty()&&price.isEmpty()){
+                Toast.makeText(CreateListingActivity.this, "Missing information for Price", Toast.LENGTH_SHORT).show();
             }
-        })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Item not added");
-                        Toast.makeText(CreateListingActivity.this, "Could not add item for sell.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
+        }
     }
 }
